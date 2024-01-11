ecc257df344148888812012d90a5dc963310ba35
ragulrathnat <113578372+ragulrathnat@users.noreply.github.com>
2024-01-11 15:14:45 +0530

    Cub_200.py

--- yolo/yolov3-tf2-master/tools/Cub_200.py
+++ yolo/yolov3-tf2-master/tools/Cub_200.py
@@ -32,7 +32,7 @@ def build_example(annotation, class_map,img_path):
     img_path = os.path.join(
         FLAGS.source, 'images', img_path)
     img_raw = open(img_path, 'rb').read()
-    print(img_raw)
+    # print(img_raw)
     # key = hashlib.sha256(img_raw).hexdigest()
 
     # width = int(annotation['size']['width'])
