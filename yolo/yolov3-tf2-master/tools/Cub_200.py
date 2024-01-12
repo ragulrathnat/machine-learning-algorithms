4fe620a456666c19e9e98b715461122eaa198088
ragulrathnat <113578372+ragulrathnat@users.noreply.github.com>
2024-01-12 12:19:45 +0530

    Cub_200.py

--- yolo/yolov3-tf2-master/tools/Cub_200.py
+++ yolo/yolov3-tf2-master/tools/Cub_200.py
@@ -64,22 +64,22 @@ def build_example(annotation, class_map,img_path):
     # views.append(obj['pose'].encode('utf8'))
 
     example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
-        'image/width': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
-        'image/filename': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
-        'image/source_id': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
-        'image/key/sha256': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
+        # 'image/height': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
+        # 'image/width': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
+        # 'image/filename': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
+        # 'image/source_id': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
+        # 'image/key/sha256': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
         'image/encoded': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw])),
-        'image/format': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
+        # 'image/format': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
         'image/object/bbox/xmin': tf.train.Feature(float_list=tf.train.FloatList(value=xmin)),
         'image/object/bbox/xmax': tf.train.Feature(float_list=tf.train.FloatList(value=xmax)),
         'image/object/bbox/ymin': tf.train.Feature(float_list=tf.train.FloatList(value=ymin)),
         'image/object/bbox/ymax': tf.train.Feature(float_list=tf.train.FloatList(value=ymax)),
         'image/object/class/text': tf.train.Feature(bytes_list=tf.train.BytesList(value=classes_text)),
-        'image/object/class/label': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
-        'image/object/difficult': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
-        'image/object/truncated': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
-        'image/object/view': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
+        # 'image/object/class/label': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
+        # 'image/object/difficult': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
+        # 'image/object/truncated': tf.train.Feature(int64_list=tf.train.Int64List(value=[22,22])),
+        # 'image/object/view': tf.train.Feature(bytes_list=tf.train.Int64List(value=[22,22])),
     }))
     return example
 
